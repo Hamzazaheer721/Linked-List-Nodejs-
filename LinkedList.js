e702ced3c8257ece3b2a2aa91870b261fd17240e
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-09-16 22:53:32 +0500

    Linked List in Javascript

---
+++ LinkedList.js
@@ -0,0 +1,41 @@
+const Node = require("./Node");
+
+// Linken list
+class LinkedList {
+  constructor() {
+    (this.head = null), (this.size = 0);
+  }
+
+  //inserting at the end
+  insert(data) {
+    //if null
+    let node = new Node(data, this.head);
+    if (this.size == 0) {
+      this.head = node;
+      this.head.next = null;
+      this.size++;
+    } else {
+      let current = this.head;
+      let node = new Node(data);
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+
+  print() {
+    console.log("hey");
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+let DList = new LinkedList();
+DList.insert(10);
+DList.insert(20);
+DList.insert(30);
+DList.print();
